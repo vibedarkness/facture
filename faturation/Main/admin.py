f361a3378571c31bf502dc0a88088a2529b23741
omzo <omzo@example.com>
2023-03-23 10:55:48 +0100

    facture

--- faturation/Main/admin.py
+++ faturation/Main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import * 
+
 # Register your models here.
+
+class AdminClient(admin.ModelAdmin):
+    list_display=('nom','email','telephone','adresse','age','sexe','ville')
+
+    
+class AdminFacture(admin.ModelAdmin):
+    list_display = ('client','user','date_heure_facture','total','dernier_modification','statut','type_facture')
+
+
+admin.site.register(Client,AdminClient)
+admin.site.register(Facture,AdminFacture)
+admin.site.register(Produit)
--- faturation/Main/models.py
+++ faturation/Main/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Client(models.Model):
+    SEXE_TYPE=(
+        ("M","Masculin"),
+        ("F","Feminin"),
+    )
+
+    nom = models.CharField(max_length=150)
+    email = models.EmailField()
+    telephone = models.CharField(max_length = 150)
+    adresse = models.CharField(max_length = 150)
+    sexe = models.CharField(max_length = 1,choices=SEXE_TYPE)
+    age = models.CharField(max_length = 12)
+    ville = models.CharField(max_length = 150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User,on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
+
+    def __str__(self):
+        return self.nom
+
+
+class Facture(models.Model):
+    Facture_Type=(
+        ('R','Reçu'),
+        ('P','Facture Proforma'),
+        ('F','Facture')
+    )
+
+    client=models.ForeignKey(Client, on_delete=models.PROTECT)
+    user=models.ForeignKey(User,on_delete=models.PROTECT)
+    date_heure_facture=models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=900, decimal_places=3)
+    dernier_modification=models.DateTimeField(null=True, blank=True)
+    statut = models.BooleanField(default=False)
+    type_facture = models.CharField(max_length = 1, choices=Facture_Type)
+
+    class Meta:
+        verbose_name = 'Facture'
+        verbose_name_plural = 'Factures'
+        
+    def __str__(self):
+        return f'{self.client.nom} {self.date_heure_facture}'
+    
+    @property
+    def get_total(self):
+        produits=self.produit_set.all()   
+
+        total=sum(produit.get_total for produit in produits) 
+
+class Produit(models.Model):
+    facture=models.ForeignKey(Facture, on_delete=models.PROTECT)
+    nom = models.CharField(max_length = 150)
+    quantite= models.IntegerField()
+    prix=models.DecimalField(max_digits=100, decimal_places=2)
+    total=models.DecimalField(max_digits=900, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Produit'
+        verbose_name_plural = 'Produits'
+
+    def __str__(self):
+        return 
+    
+    @property
+    def get_total(self):
+
+        total=self.prix * self.quantite
+
+    
+    
+    
+    
+    
